dbda1e26c2f635b809912fe4f734de88eb4bf7b2
Satrinus <anamcharazik@gmail.com>
2017-02-20 18:12:02 +0200

    init commit

---
+++ less5/src/less5/Main.java
@@ -0,0 +1,23 @@
+package less5;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int mass[] = {23,123,123,21,3};
+//		for (int i = 0; i < mass.length; i++) {
+//			System.out.println(mass[i]);
+//		}
+		
+		for(int iter : mass){
+			System.out.println(iter);
+		}
+				
+				
+		}
+		
+		
+	}
+
+
